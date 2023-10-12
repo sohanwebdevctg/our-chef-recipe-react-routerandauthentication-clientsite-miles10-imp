d42cb19b0a5df3cd72b981051e201eecad3b2836
sohanwebdevctg <amirhossainsohan0@gmail.com>
2023-10-12 07:58:36 +0600

    adding firebase authentication

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAhhR5M2MAh-lq5a6N08rXPNWXNZxpDGRQ",
+  authDomain: "our-chef-recipe-54354.firebaseapp.com",
+  projectId: "our-chef-recipe-54354",
+  storageBucket: "our-chef-recipe-54354.appspot.com",
+  messagingSenderId: "247687161921",
+  appId: "1:247687161921:web:313d886ed609803e7ffec0"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
